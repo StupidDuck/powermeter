010768f89e568770c3f403478bb367ca6d447c21
Lescot Gilles <lescotg@hotmail.com>
2019-06-06 16:18:07 +0200

    ...

---
+++ core/data_access/__init__.py
@@ -0,0 +1,22 @@
+from .db import get_cursor
+
+with get_cursor() as c:
+    c.execute("""SELECT table_name
+                        FROM information_schema.tables
+                        WHERE table_name='meters';""")
+    if c.fetchone() is None:
+        c.execute("""CREATE TABLE meters (
+                        id SERIAL PRIMARY KEY,
+                        name VARCHAR(30) NOT NULL,
+                        user_id text NOT NULL
+                        );""")
+    c.execute("""SELECT table_name
+                        FROM information_schema.tables
+                        WHERE table_name='indexes';""")
+    if c.fetchone() is None:
+        c.execute("""CREATE TABLE indexes (
+                        id SERIAL PRIMARY KEY,
+                        date DATE NOT NULL,
+                        value REAL NOT NULL,
+                        meter_id integer REFERENCES meters (id)
+                        );""")
--- core/data_access/db.py
+++ core/data_access/db.py
@@ -10,24 +10,3 @@ def get_cursor():
     yield connection.cursor()
     connection.commit()
     connection.close()
-
-with get_cursor() as c:
-    c.execute("""SELECT table_name
-                        FROM information_schema.tables
-                        WHERE table_name='meters';""")
-    if c.fetchone() is None:
-        c.execute("""CREATE TABLE meters (
-                        id SERIAL PRIMARY KEY,
-                        name VARCHAR(30) NOT NULL,
-                        user_id text NOT NULL
-                        );""")
-    c.execute("""SELECT table_name
-                        FROM information_schema.tables
-                        WHERE table_name='indexes';""")
-    if c.fetchone() is None:
-        c.execute("""CREATE TABLE indexes (
-                        id SERIAL PRIMARY KEY,
-                        date DATE NOT NULL,
-                        value REAL NOT NULL,
-                        meter_id integer REFERENCES meters (id)
-                        );""")
